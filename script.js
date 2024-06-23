8aad4b7df7826b067aa366040b5f34f693827247
Zynalex9 <alzn952@gmail.com>
2024-06-24 01:43:34 +0500

    Section 2 added

--- script.js
+++ script.js
@@ -1,46 +1,6 @@
-var tl = gsap.timeline();
 const reelCursor = document.querySelector(".cursor");
-const scroll = new LocomotiveScroll({
-  el: document.querySelector("#wrapper"),
-  smooth: true,
-});
-gsap.registerPlugin(ScrollTrigger);
-
-// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
-
-const locoScroll = new LocomotiveScroll({
-  el: document.querySelector("#wrapper"),
-  smooth: true,
-});
-// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
-locoScroll.on("scroll", ScrollTrigger.update);
-
-// tell ScrollTrigger to use these proxy methods for the "#wrapper" element since Locomotive Scroll is hijacking things
-ScrollTrigger.scrollerProxy("#wrapper", {
-  scrollTop(value) {
-    return arguments.length
-      ? locoScroll.scrollTo(value, 0, 0)
-      : locoScroll.scroll.instance.scroll.y;
-  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
-  getBoundingClientRect() {
-    return {
-      top: 0,
-      left: 0,
-      width: window.innerWidth,
-      height: window.innerHeight,
-    };
-  },
-  // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
-  pinType: document.querySelector("#wrapper").style.transform
-    ? "transform"
-    : "fixed",
-});
-
-// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll.
-ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
-
-// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
-ScrollTrigger.refresh();
+const viewText = document.querySelector("#viewText")
+var tl = gsap.timeline();
 
 document
   .querySelector(".hero-section .left-side video")
@@ -54,6 +14,7 @@ document
     reelCursor.style.top = `${30}%`;
     reelCursor.style.left = `${30}%`;
   });
+
 tl.from(".hero-section .left-side video", { opacity: 0, duration: 0.3 })
   .from(".hero-section .right-side .section-1 h3", {
     opacity: 0,
@@ -79,7 +40,7 @@ gsap.to(".hero-section .right-side .section-3 h1", {
   transform: "translateX(70%)",
 
   scrollTrigger: {
-    trigger: ".hero-section",
+    trigger: "#wrapper",
     scroller: "body",
     start: "top 0",
     end: "top -150%",
@@ -92,7 +53,8 @@ gsap.to(".hero-section .right-side .section-2 .before", {
   left: 0,
   duration: 2,
   scrollTrigger: {
-    trigger: ".hero-section",
+    trigger: "#wrapper",
+   
     scroller: "body",
     start: "top .2%",
     end: "bottom 50%",
@@ -104,7 +66,8 @@ gsap.to(".hero-section .right-side .section-3 .before", {
   left: 0,
   duration: 2,
   scrollTrigger: {
-    trigger: ".hero-section",
+    trigger: "#wrapper",
+   
     scroller: "body",
     start: "top .3%",
     end: "bottom 50%",
